0dd8508a672a9e5ab7b3423e71c39d6fbe9f734c
Oreoluwa Adeleke <58007764+Oreos3@users.noreply.github.com>
2022-10-28 14:52:18 -0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,28 @@
+##from django.conf import settings
+##if not settings.configured:
+##    settings.configure(DEBUG=True)
+
+
+
+
+from django.db import models
+
+
+# Create your models here.
+
+class Artiste(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  Age = models.IntegerField(default=0)
+
+
+class Song(models.Model):
+  title = models.CharField(max_length=255)
+  likes = models.IntegerField(default=0) ##number
+  artiste_id = models.IntegerField(default=0)  ## integer
+  date_released = models.DateField(auto_now=True)
+
+
+class Lyric(models.Model):
+  content = models.CharField(max_length=255)
+  song_id = models.IntegerField(default=0)
